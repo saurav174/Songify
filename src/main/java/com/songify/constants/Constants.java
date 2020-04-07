b565e76fa82c0bd3f2bf0df55183ffbd4c5f2872
Saurav Kumar <saurav.kumar@bharatpe.com>
2020-04-07 16:20:04 +0530

    adding authentication check

--- src/main/java/com/songify/constants/Constants.java
+++ src/main/java/com/songify/constants/Constants.java
@@ -10,4 +10,5 @@ public class Constants {
     public static final String REGISTRATION_FAILED_USER_EXIST = "User Registration failed user with userId already exists!";
     public static final String REGISTRATION_FAILED_PASSWORD_INVALID = "User Registration failed password is invalid!";
     public static final String REGISTRATION_FAILED_EMAIL_INVALID = "User Registration failed email is invalid!";
+    public static final String AUTHENTICATION_FAILED = "User Authentication failed userId or Password is invalid!";
 }
--- src/main/java/com/songify/services/LoginService.java
+++ src/main/java/com/songify/services/LoginService.java
@@ -3,6 +3,7 @@ package com.songify.services;
 
 import com.songify.constants.Constants;
 import com.songify.dao.UserDao;
+import com.songify.dto.CommonResponseDTO;
 import com.songify.dto.LoginResponseDTO;
 import com.songify.entities.User;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +20,12 @@ public class LoginService {
     {
         User user = userDao.findByUserIDAAndPassword(userId,password);
         LoginResponseDTO loginResponseDTO = new LoginResponseDTO();
+        if (user==null)
+        {
+            loginResponseDTO.setSuccess(false);
+            loginResponseDTO.setMsg(Constants.AUTHENTICATION_FAILED);
+            return  loginResponseDTO;
+        }
         loginResponseDTO.setSuccess(true);
         loginResponseDTO.setMsg(Constants.LOGIN_SUCCESS);
         loginResponseDTO.setName(user.getName());
